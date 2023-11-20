c2e03f7884b6422b0def2e18a8c39b6d0fae2b12
Theophilus <isahtheophilus@gmail.com>
2023-11-20 21:41:51 +0100

    feat: add colors to tailwind config

--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -24,8 +24,8 @@ const CustomDialog = ({
         <Dialog.Root open={isOpen} onOpenChange={onDismiss}>
             <Dialog.Trigger asChild />
             <Dialog.Portal>
-                <Dialog.Overlay className="bg-black opacity-30 data-[state=open]:animate-overlayShow fixed inset-0" />
-                <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-black text-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+                <Dialog.Overlay className="bg-black-100 opacity-30 data-[state=open]:animate-overlayShow fixed inset-0" />
+                <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-black-100 text-white-100 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
                     <Dialog.Title
                         className={`m-0 text-[17px] font-medium ${
                             titleClassName || ""
@@ -39,7 +39,7 @@ const CustomDialog = ({
                     {children}
                     <Dialog.Close asChild>
                         <button
-                            className="absolute -top-10 -right-7 bg-black text-white hover:bg-slate-800 focus:shadow-violet7 inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+                            className="absolute -top-10 -right-7 bg-black-100 text-white hover:bg-slate-800 focus:shadow-black-500 inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
                             aria-label="Close"
                         >
                             <Cross2Icon />
